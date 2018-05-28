bfefe465a58c8b962ceeaafc922e49feca65d182
NrjMeyer <nrjmeyer@gmail.com>
2018-05-28 13:02:03 +0300

    Seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_28_084438) do
+ActiveRecord::Schema.define(version: 2018_05_28_095728) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.decimal "price"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
