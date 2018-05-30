1c6e8676cdf17e0c8a92da403ab87c5c6627e44d
naojulius <naojuliusforslack@gmail.com>
2018-05-30 13:18:20 +0300

    profile, edit account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+before_action :configure_permited_parameters, if: :devise_controller?
+ def configure_permited_parameters
+      
+      devise_parameter_sanitizer.permit(:sign_up) { |s| s.permit( :fullname, :email, :password )  }
+
+ end
+
+
 end
