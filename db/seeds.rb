907e0ab000ebd75d0670884a6c52412371c2b0bf
NrjMeyer <nrjmeyer@gmail.com>
2018-05-30 11:55:09 +0300

    Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,137 +6,137 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-cat = Item.create(title: 'cat', description: 'so cute', price: 2.4, image_url: 'images/cat.jpg')
+cat = Item.create(title: 'cat', description: 'Là haut', price: 2.4, image_url: 'cat.jpg')
 cat.save
 
-cat2 = Item.create(title: 'cat2', description: 'stupif cat', price: 1.4, image_url: 'images/cat2.jpg')
+cat2 = Item.create(title: 'cat2', description: 'En studio', price: 1.4, image_url: 'cat2.jpg')
 cat2.save
 
-cat4 = Item.create(title: 'cat4', description: 'mon chatton bien habillé', price: 1.2, image_url: 'images/cat4.jpg')
+cat4 = Item.create(title: 'cat4', description: 'Je porte une chaussure', price: 1.2, image_url: 'cat4.jpg')
 cat4.save
 
-cat3 = Item.create(title: 'cover-image-1', description: 'so little cat', price: 0.9, image_url: 'images/cover-image-1.jpg')
+cat3 = Item.create(title: 'cover-image-1', description: 'Dans la nature', price: 0.9, image_url: 'cover-image-1.jpg')
 cat3.save
 
-cat5 = Item.create(title: 'item-10', description: 'Le brave', price: 1.4, image_url: 'images/item-10.jpg')
+cat5 = Item.create(title: 'item-10', description: 'So cute', price: 1.4, image_url: 'item-10.jpg')
 cat5.save
 
-cat6 = Item.create(title: 'item-11', description: 'ils sont adorable', price: 1.5, image_url: 'images/item-11.jpg')
+cat6 = Item.create(title: 'item-11', description: 'Dans mon lit', price: 1.5, image_url: 'item-11.jpg')
 cat6.save
 
-cat7 = Item.create(title: 'item-12', description: 'so strange', price: 1.7, image_url: 'images/item-12.jpeg')
+cat7 = Item.create(title: 'item-12', description: 'Trop chouuuu', price: 1.7, image_url: 'item-12.jpeg')
 cat7.save
 
-cat8 = Item.create(title: 'item-13', description: 'I can play too', price: 1.4, image_url: 'images/item-13.jpg')
+cat8 = Item.create(title: 'item-13', description: 'Twins', price: 1.4, image_url: 'item-13.jpg')
 cat8.save
 
-cat9 = Item.create(title: 'item-14', description: 'regarder-le, il est si gentil', price: 1.8, image_url: 'images/item-14.jpg')
+cat9 = Item.create(title: 'item-14', description: 'regarder-le, il est si gentil', price: 1.8, image_url: 'item-14.jpg')
 cat9.save
 
-cat10 = Item.create(title: 'item-15', description: 'Un chatton qui sait chassé', price: 2.4, image_url: 'images/item-15jpeg')
+cat10 = Item.create(title: 'item-15', description: 'Un chatton pensif', price: 2.4, image_url: 'item-15jpeg')
 cat10.save
 
-cat11 = Item.create(title: 'item-16', description: 'adorable', price: 2.6, image_url: 'images/item-16.jpg')
+cat11 = Item.create(title: 'item-16', description: 'Inoffensif', price: 2.6, image_url: 'item-16.jpg')
 cat11.save
 
-cat12 = Item.create(title: 'item-9', description: 'trop chouuuuu', price: 3.4, image_url: 'images/item-9.jpeg')
+cat12 = Item.create(title: 'item-9', description: 'Repond-moi!', price: 3.4, image_url: 'item-9.jpeg')
 cat12.save
 
-cat13 = Item.create(title: 'cat-10', description: 'mes petits anges', price: 2.7, image_url: 'images/product/cat-10.jpg')
+cat13 = Item.create(title: 'cat-10', description: 'Shoot studio', price: 2.7, image_url: '/product/cat-10.jpg')
 cat13.save
 
-cat14 = Item.create(title: 'cat-11', description: 'Il jouent comme des petits heureux', price: 1.8, image_url: 'images/product/cat-11.jpg')
+cat14 = Item.create(title: 'cat-11', description: 'Trop adorable', price: 1.8, image_url: '/product/cat-11.jpg')
 cat14.save
 
-cat15 = Item.create(title: 'cat-12', description: 'son première fois avec les souris', price: 3.4, image_url: 'images/product/cat-12.jpg')
+cat15 = Item.create(title: 'cat-12', description: 'Comme ils sont trop mignons', price: 3.4, image_url: '/product/cat-12.jpg')
 cat15.save
 
-cat16 = Item.create(title: 'cat-15', description: 'le parresseux', price: 2.0, image_url: 'images/product/cat-15.jpg')
+cat16 = Item.create(title: 'cat-15', description: 'So cute', price: 2.0, image_url: '/product/cat-15.jpg')
 cat16.save
 
-cat17 = Item.create(title: 'cat-16', description: 'Ils dorment comme un bébé', price: 2.1, image_url: 'images/product/cat-16.jpg')
+cat17 = Item.create(title: 'cat-16', description: 'Ils dorment comme un bébé', price: 2.1, image_url: '/product/cat-16.jpg')
 cat17.save
 
-cat18 = Item.create(title: 'cat-18', description: 'Il rêve.... trop chouuuu', price: 2.2, image_url: 'images/product/cat-18.jpg')
+cat18 = Item.create(title: 'cat-18', description: 'Impressioné', price: 2.2, image_url: '/product/cat-18.jpg')
 cat18.save
 
-cat19 = Item.create(title: 'cat-19', description: 'Ils jouent là.... super planque', price: 2.4, image_url: 'images/product/cat-19.jpg')
+cat19 = Item.create(title: 'cat-19', description: 'Metaleux...', price: 2.4, image_url: '/product/cat-19.jpg')
 cat19.save
 
-cat20 = Item.create(title: 'cat-2', description: 'Je porte une chaussure', price: 2.3, image_url: 'images/product/cat-2.jpg')
+cat20 = Item.create(title: 'cat-2', description: 'Haut les mains!', price: 2.3, image_url: '/product/cat-2.jpg')
 cat20.save
 
-cat21 = Item.create(title: 'cat-21', description: 'adorable', price: 2.6, image_url: 'images/product/cat-21.jpg')
+cat21 = Item.create(title: 'cat-21', description: 'Figés', price: 2.6, image_url: '/product/cat-21.jpg')
 cat21.save
 
-cat22 = Item.create(title: 'cat-22', description: 'trop chouuuuu', price: 3.4, image_url: 'images/product/cat-22.jpg')
+cat22 = Item.create(title: 'cat-22', description: 'Entre deux...copains', price: 3.4, image_url: '/product/cat-22.jpg')
 cat22.save
 
-cat23 = Item.create(title: 'cat-23', description: 'mes petits anges', price: 2.7, image_url: 'images/product/cat-23.jpg')
+cat23 = Item.create(title: 'cat-23', description: 'Il fait quoi là?', price: 2.7, image_url: '/product/cat-23.jpg')
 cat23.save
 
-cat24 = Item.create(title: 'cat-24', description: 'Il jouent comme des petits heureux', price: 1.8, image_url: 'images/product/cat-24.jpg')
+cat24 = Item.create(title: 'cat-24', description: 'Ils ont un petit soucis', price: 1.8, image_url: '/product/cat-24.jpg')
 cat24.save
 
-cat25 = Item.create(title: 'cat-25', description: 'son première fois avec les souris', price: 3.4, image_url: 'images/product/cat-25.jpg')
+cat25 = Item.create(title: 'cat-25', description: 'Oh...le pauvre', price: 3.4, image_url: '/product/cat-25.jpg')
 cat25.save
 
-cat26 = Item.create(title: 'cat-27', description: 'le parresseux', price: 2.0, image_url: 'images/product/cat-27.jpg')
+cat26 = Item.create(title: 'cat-27', description: 'Blue Eyes', price: 2.0, image_url: '/product/cat-27.jpg')
 cat26.save
 
-cat27 = Item.create(title: 'cat-28', description: 'Ils dorment comme un bébé', price: 2.1, image_url: 'images/product/cat-28.jpg')
+cat27 = Item.create(title: 'cat-28', description: 'Eh...ne me regarde pas!', price: 2.1, image_url: '/product/cat-28.jpg')
 cat27.save
 
-cat28 = Item.create(title: 'cat-5', description: 'Il rêve.... trop chouuuu', price: 2.2, image_url: 'images/product/cat-5.jpg')
+cat28 = Item.create(title: 'cat-5', description: 'Il rêve.... trop chouuuu', price: 2.2, image_url: '/product/cat-5.jpg')
 cat28.save
 
-cat29 = Item.create(title: 'cat_1', description: 'Ils jouent là.... super planque', price: 2.4, image_url: 'images/product/cat_1.jpeg')
+cat29 = Item.create(title: 'cat_1', description: 'Tape là...', price: 2.4, image_url: '/product/cat_1.jpeg')
 cat29.save
 
-cat30 = Item.create(title: 'cat_2', description: 'Je porte une chaussure', price: 2.3, image_url: 'images/product/cat_2.jpg')
+cat30 = Item.create(title: 'cat_2', description: 'Petit souris', price: 2.3, image_url: '/product/cat_2.jpg')
 cat30.save
 
-cat31 = Item.create(title: 'cat_3', description: 'adorable', price: 2.6, image_url: 'images/product/cat_3.jpg')
+cat31 = Item.create(title: 'cat_3', description: 'Little cat', price: 2.6, image_url: '/product/cat_3.jpg')
 cat31.save
 
-cat32 = Item.create(title: 'cat_4', description: 'trop chouuuuu', price: 3.4, image_url: 'images/product/cat_4.jpg')
+cat32 = Item.create(title: 'cat_4', description: 'Don\'t drink me please', price: 3.4, image_url: '/product/cat_4.jpg')
 cat32.save
 
-cat33 = Item.create(title: 'cat_6', description: 'mes petits anges', price: 2.7, image_url: 'images/product/cat_6.jpg')
+cat33 = Item.create(title: 'cat_6', description: 'Prend moi dans tes bras...', price: 2.7, image_url: '/product/cat_6.jpg')
 cat33.save
 
-cat34 = Item.create(title: 'cat_7', description: 'Il jouent comme des petits heureux', price: 1.8, image_url: 'images/product/cat_7.jpg')
+cat34 = Item.create(title: 'cat_7', description: 'Hey....', price: 1.8, image_url: '/product/cat_7.jpg')
 cat34.save
 
-cat35 = Item.create(title: 'cat_71', description: 'son première fois avec les souris', price: 3.4, image_url: 'images/product/cat_7.jng')
+cat35 = Item.create(title: 'cat_71', description: 'Chat....misterieux', price: 3.4, image_url: '/product/cat_7.png')
 cat35.save
 
-cat36 = Item.create(title: 'cat_8', description: 'le parresseux', price: 2.0, image_url: 'images/product/cat_8.jpg')
+cat36 = Item.create(title: 'cat_8', description: 'le parresseux', price: 2.0, image_url: '/product/cat_8.jpg')
 cat36.save
 
-cat37 = Item.create(title: 'cat_9', description: 'Ils dorment comme un bébé', price: 2.1, image_url: 'images/product/cat_9.jpg')
+cat37 = Item.create(title: 'cat_9', description: 'Joue avec la bal', price: 2.1, image_url: '/product/cat_9.jpg')
 cat37.save
 
-cat38 = Item.create(title: 'item-10', description: 'Il rêve.... trop chouuuu', price: 2.2, image_url: 'images/product/item-10.jpg')
+cat38 = Item.create(title: 'item-10', description: 'Je suis levé', price: 2.2, image_url: '/product/item-10.jpg')
 cat38.save
 
-cat39 = Item.create(title: 'item-11', description: 'Ils jouent là.... super planque', price: 2.4, image_url: 'images/product/item-11.jpg')
+cat39 = Item.create(title: 'item-11', description: 'Ils jouent là.... super planque', price: 2.4, image_url: '/product/item-11.jpg')
 cat39.save
 
-cat40 = Item.create(title: 'item-12', description: 'Je porte une chaussure', price: 2.3, image_url: 'images/product/item-12.jpeg')
+cat40 = Item.create(title: 'item-12', description: 'Je dors avec mon doudou', price: 2.3, image_url: '/product/item-12.jpeg')
 cat40.save
 
-cat41 = Item.create(title: 'item-13', description: 'adorable', price: 2.6, image_url: 'images/product/item-13.jpg')
+cat41 = Item.create(title: 'item-13', description: 'So cute twins', price: 2.6, image_url: '/product/item-13.jpg')
 cat41.save
 
-cat42 = Item.create(title: 'item-14', description: 'trop chouuuuu', price: 3.4, image_url: 'images/product/item-14.jpg')
+cat42 = Item.create(title: 'item-14', description: 'Je me lave', price: 3.4, image_url: '/product/item-14.jpg')
 cat42.save
 
-cat43 = Item.create(title: 'item-15', description: 'mes petits anges', price: 2.7, image_url: 'images/product/item-15.jpeg')
+cat43 = Item.create(title: 'item-15', description: 'what\'s wrong???', price: 2.7, image_url: '/product/item-15.jpeg')
 cat43.save
 
-cat44 = Item.create(title: 'item-16', description: 'Il jouent comme des petits heureux', price: 1.8, image_url: 'images/product/item-16.jpg')
+cat44 = Item.create(title: 'item-16', description: 'Petit tigre', price: 1.8, image_url: '/product/item-16.jpg')
 cat44.save
 
-cat45 = Item.create(title: 'item-9', description: 'son première fois avec les souris', price: 3.4, image_url: 'images/product/item-9.jpeg')
+cat45 = Item.create(title: 'item-9', description: 'AHH...laisse moi parler!', price: 3.4, image_url: '/product/item-9.jpeg')
 cat45.save
