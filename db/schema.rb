bd90c88d1735bc6fae1d7cc1e006370777af0efe
tinosam <yvestinos@gmail.com>
2018-05-30 14:25:38 +0100

    tinio

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_30_061927) do
+ActiveRecord::Schema.define(version: 2018_05_30_131240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
